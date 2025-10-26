99fe4420af6435f0d1046e0684e94434b58ae6ee
irishcamilleipili <ipiliirish@gmail.com>
2025-10-26 14:08:34 +0800

    Intial pani

--- items/views.py
+++ items/views.py
@@ -4,26 +4,43 @@ from .forms import ItemForm
 
 
 def home_view(request):
-    items = Item.objects.all()
-    return render(request, 'invApp/home.html')
+    items = Item.objects.all().order_by('-date_reported')
+    return render(request, 'invApp/home.html', {'items': items})
+
 
 def item_list(request):
     items = Item.objects.all().order_by('-date_reported')
     return render(request, 'invApp/item_list.html', {'items': items})
 
+
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     return render(request, 'invApp/item_detail.html', {'item': item})
 
+
 def item_create(request):
+    """
+    Handles the Found Item creation and listing
+    The category is automatically set to 'found'
+    """
+    # Show only found items on the right side
+    items = Item.objects.filter(category='found').order_by('-date_reported')
+
     if request.method == "POST":
         form = ItemForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('item_list')
+            item = form.save(commit=False)
+            item.category = 'found'  # 👈 Automatically tag it as found
+            item.save()
+            return redirect('item_create')  # Reload the page to show the new item
     else:
         form = ItemForm()
-    return render(request, 'invApp/item_form.html', {'form': form})
+
+    return render(request, 'invApp/item_form.html', {
+        'form': form,
+        'items': items
+    })
+
 
 def item_update(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -35,6 +52,7 @@ def item_update(request, pk):
     else:
         form = ItemForm(instance=item)
     return render(request, 'invApp/item_form.html', {'form': form})
+
 
 def item_delete(request, pk):
     item = get_object_or_404(Item, pk=pk)
